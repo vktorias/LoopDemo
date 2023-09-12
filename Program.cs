4002148c36bea5a01fac6fedf63a0204144cffef
vikto <vikto@VICKY>
2023-09-12 15:22:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualBasic;
+
+namespace LoopDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //    int myNumber = 0;
+
+            //    while (myNumber <= 5)
+            //    {
+            //        Console.WriteLine(myNumber);
+            //        myNumber++;
+            //    }
+
+
+
+            //string myPassword = "12345";
+            //string userInput = "";
+
+            //do
+            //{
+            //    Console.WriteLine("Skriv lösenord:");
+            //    userInput = Console.ReadLine();
+
+            //} while (userInput != myPassword);
+
+
+            //int Number = 0;
+
+            //Console.WriteLine("While loop starts here:");
+            //while (Number <= 10)
+
+            //{
+            //    Console.WriteLine(Number);
+            //    Number++;
+            //}
+
+            //Console.WriteLine("For loop starts here:");
+            //for (int Number1 = 0; Number1 <= 10; Number1++)
+            //{
+            //    Console.WriteLine(Number1);
+            //}
+
+
+            //Skapar en array med 3 värden. 
+
+            string[] users = { "Aldor", "Reidar", "Anders" };
+
+            for (int i = 0; i < users.Length; i++)
+            {
+                Console.WriteLine(users[i]);
+            }
+
+            foreach (string user in users)
+            {
+                Console.WriteLine(user);
+            }
+        }
+    }
+}
